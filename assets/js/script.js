bcf49a250ecf0abd977144373e041dba17c90d15
Local MM <73925149+mymy-4242@users.noreply.github.com>
2020-12-14 23:14:33 -0800

    add tasks and task types to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
-    tasksToDoEl.appendChild(listItemEl);
 
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h3/><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire item to list
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
